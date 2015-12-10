d99601c3cd24b213dcf1338dff5a4bb3f15f58b1
John Trowbridge <trown@redhat.com>
2015-12-10 10:20:06 -0500

    Add ability to specify release to quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -4,7 +4,9 @@ set -ex
 
 virtualenv .quickstart --system-site-packages
 source .quickstart/bin/activate
-pip install git+https://github.com/trown/tripleo-quickstart.git@master#egg=tripleo-quickstart
+pip install git+https://github.com/trown/tripleo-quickstart.git@mitaka#egg=tripleo-quickstart
 # tripleo-quickstart requires Ansible 2.0
 pip install git+https://github.com/ansible/ansible.git@v2.0.0-0.6.rc1#egg=ansible
-ansible-playbook -vv .quickstart/usr/local/share/tripleo-quickstart/playbooks/quickstart.yml
+
+RELEASE=${1:-liberty}
+ansible-playbook -vv .quickstart/usr/local/share/tripleo-quickstart/playbooks/quickstart-$RELEASE.yml
