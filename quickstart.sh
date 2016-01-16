be51e47da51f6b57da3fa55cdd1119b0aea5fac6
John Trowbridge <trown@redhat.com>
2016-01-16 16:55:19 -0500

    Various fixes from old github repo
    Use local_working_dir everywhere meant for localhost
    
    Update requirements.txt for ansible 2.0 release
    
    Fix quickstart playbooks for inventory refactor
    
    Put defaults for setup and teardown under libvirt role
    
    Change-Id: I68b1f0cc6a51902a56fb7e8f4561e70a9afb9652

--- quickstart.sh
+++ quickstart.sh
@@ -4,13 +4,16 @@ set -ex
 
 virtualenv .quickstart --system-site-packages
 source .quickstart/bin/activate
-pip install -I pbr
-pip install git+https://github.com/trown/tripleo-quickstart.git@master#egg=tripleo-quickstart
-# tripleo-quickstart requires Ansible 2.0
-pip install -I git+https://github.com/ansible/ansible.git@v2.0.0-0.6.rc1#egg=ansible
-
+pushd .quickstart
+git clone https://github.com/trown/tripleo-quickstart.git
+pushd tripleo-quickstart
+python setup.py install
+popd
+popd
 export ANSIBLE_CONFIG=.quickstart/usr/local/share/tripleo-quickstart/ansible.cfg
 export ANSIBLE_INVENTORY=.quickstart/hosts
+
+echo "ssh_args = -F $PWD/.quickstart/ssh.config.ansible" >> $ANSIBLE_CONFIG
 
 RELEASE=${1:-liberty}
 ansible-playbook -vv .quickstart/usr/local/share/tripleo-quickstart/playbooks/quickstart-$RELEASE.yml
