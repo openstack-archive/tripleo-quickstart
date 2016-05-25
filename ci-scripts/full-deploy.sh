openstack-archive/tripleo-quickstart

commit 6926a86c3088f76e00397e58862070ba5e2851e6

move most things out from the playbooks dir, and use config for release info
* move the roles, libraries and config to a top level dir
* adjust the ansible.cfg to account for the new role path
* create a config directory
  config/release  <- only for release and image_url info
  config/general_config <- everything else
* update collect-logs role path
* add ci-scripts/full-minimal.sh so that jjb will not have
to be updated after this.

Change-Id: I2e6129553bca253fa47ce56bfcbc2f244e74409e

ci-scripts/full-minimal.sh
@@ -1,0 +1,12 @@
+#!/bin/bash
+# Full CI test that runs quickstart.sh with only the
+# release argument
+
+set -eux
+
+bash $WORKSPACE/tripleo-quickstart/quickstart.sh \
+--tags all \
+--config $WORKSPACE/tripleo-quickstart/config/general_config/minimal.yml \
+--working-dir $WORKSPACE/ \
+--no-clone \
+$VIRTHOST $RELEASE

commit ee869f2c15a00f353952013d534cd4b693612ccc

Fix full deploy CI
6926a86c3088f76e00397e58862070ba5e2851e6 broke full
deploy CI, because I forgot to run it before merging.

We also don't need full-minimal.sh and full-deploy.sh.
Keeping full-deploy.sh since it is consistent with
the other CI scripts.

Change-Id: I88deb44d4bec1f740d57ac523b6c1667713f90a1

ci-scripts/full-deploy.sh
@@ -27,8 +27,8 @@ export ANSIBLE_SSH_CONTROL_PATH=$socketdir/%%h-%%r
 
 bash $WORKSPACE/tripleo-quickstart/quickstart.sh \
 --tags all \
---undercloud-image-url "http://artifacts.ci.centos.org/artifacts/rdo/images/$RELEASE/$BUILD_SYS/$LOCATION/undercloud.qcow2" \
---config $WORKSPACE/tripleo-quickstart/playbooks/centosci/$CONFIG.yml \
+-e image_url="http://artifacts.ci.centos.org/artifacts/rdo/images/$RELEASE/$BUILD_SYS/$LOCATION/undercloud.qcow2" \
+--config $WORKSPACE/tripleo-quickstart/config/general_config/$CONFIG.yml \
 --working-dir $WORKSPACE/ \
 --no-clone \
 $VIRTHOST $RELEASE

ci-scripts/full-minimal.sh
@@ -1,12 +1,0 @@
-#!/bin/bash
-# Full CI test that runs quickstart.sh with only the
-# release argument
-
-set -eux
-
-bash $WORKSPACE/tripleo-quickstart/quickstart.sh \
---tags all \
---config $WORKSPACE/tripleo-quickstart/config/general_config/minimal.yml \
---working-dir $WORKSPACE/ \
---no-clone \
-$VIRTHOST $RELEASE

ci-scripts/images.sh
@@ -34,7 +34,7 @@ if [ "$JOB_TYPE" = "gate" ] || [ "$JOB_TYPE" = "periodic" ]; then
     --extra-vars image_url="file:///var/lib/oooq-images/undercloud.qcow2" \
     --extra-vars artib_release=$RELEASE \
     --extra-vars artib_build_system=$BUILD_SYS \
-    --extra-vars @$WORKSPACE/tripleo-quickstart/playbooks/centosci/$CONFIG.yml
+    --extra-vars @$WORKSPACE/tripleo-quickstart/config/general_config/$CONFIG.yml
 elif [ "$JOB_TYPE" = "promote" ]; then
     $anscmd -i local_hosts \
     $WORKSPACE/tripleo-quickstart/playbooks/build-images.yml \