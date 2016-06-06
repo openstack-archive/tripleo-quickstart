5e0a381ff953b3f7700fb1efa8ba1028295467dd
Attila Darazs <adarazs@redhat.com>
2016-06-06 17:42:06 +0200

    Gate ansible-role-tripleo-* roles
    * add ci script and a playbook to gate the roles
    * script is compatible with future periodic and promote jobs
    * add a playbook that uses the roles
    
    Change-Id: Ib1d48d6a79ff94d2a08ea5eef12bb11cdbacf82b

---
+++ ci-scripts/roles-deploy.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Used to gate ansible-role-tripleo-* repositories
+# Usage: gate-roles.sh <release> <build_system> <config> <job_type>
+set -eux
+
+# CONFIG and JOB_TYPE are not used here, but kept for
+# consistency with other jobs to make JJB cleaner.
+RELEASE=$1
+BUILD_SYS=$2
+CONFIG=$3
+JOB_TYPE=$4
+
+if [ "$JOB_TYPE" = "gate" ] || [ "$JOB_TYPE" = "periodic" ]; then
+    LOCATION='stable'
+elif [ "$JOB_TYPE" = "promote" ]; then
+    LOCATION='testing'
+else
+    echo "Job type must be one of gate, periodic, or promote"
+    exit 1
+fi
+
+export SSH_CONFIG=$WORKSPACE/ssh.config.ansible
+export ANSIBLE_SSH_ARGS="-F ${SSH_CONFIG}"
+# (trown) This is so that we ensure separate ssh sockets for
+# concurrent jobs. Without this, two jobs running in parallel
+# would try to use the same undercloud-stack socket.
+socketdir=$(mktemp -d /tmp/sockXXXXXX)
+export ANSIBLE_SSH_CONTROL_PATH=$socketdir/%%h-%%r
+
+if [ "$JOB_TYPE" = "gate" ]; then
+    # set up the gated repos and modify the requirements file to use them
+    bash $WORKSPACE/tripleo-quickstart/quickstart.sh \
+        --working-dir $WORKSPACE/ \
+        --no-clone \
+        --bootstrap \
+        --requirements $WORKSPACE/tripleo-quickstart/quickstart-role-requirements.txt \
+        --playbook gate-roles.yml \
+        $VIRTHOST $RELEASE
+
+    # once more to let the gating role be gated as well
+    bash $WORKSPACE/tripleo-quickstart/quickstart.sh \
+        --working-dir $WORKSPACE/ \
+        --no-clone \
+        --bootstrap \
+        --requirements $WORKSPACE/tripleo-quickstart/quickstart-role-requirements.txt \
+        --playbook gate-roles.yml \
+        $VIRTHOST $RELEASE
+fi
+
+# run the gate job using gated roles and the role based playbook
+bash $WORKSPACE/tripleo-quickstart/quickstart.sh \
+    --working-dir $WORKSPACE/ \
+    --no-clone \
+    --bootstrap \
+    --requirements $WORKSPACE/tripleo-quickstart/quickstart-role-requirements.txt \
+    -e undercloud_image_url="http://artifacts.ci.centos.org/artifacts/rdo/images/$RELEASE/$BUILD_SYS/$LOCATION/undercloud.qcow2" \
+    --config $WORKSPACE/config/general_config/$CONFIG.yml \
+    --playbook tripleo-roles.yml \
+    --tags all \
+    $VIRTHOST $RELEASE
