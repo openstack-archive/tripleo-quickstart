2e426e7046af31162e3f44626ec26ff40ea2709e
Wesley Hayutin <weshayutin@gmail.com>
2016-06-06 11:45:05 -0400

    add scripts required to test ooo-usbkey workflow
    Change-Id: I1b6dbb58963c4a8f48c441eb4201e57eb3507e21

---
+++ ci-scripts/ooo-usbkey.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# CI test that tests the USB key method of using tripleo-quickstart
+# Usage: basic.sh <release> <build_system> <config> <job_type>
+set -eux
+
+# CONFIG and JOB_TYPE are not used here, but kept for
+# consistency with other jobs to make JJB cleaner.
+RELEASE=$1
+BUILD_SYS=$2
+CONFIG=$3
+JOB_TYPE=$4
+
+URL=http://artifacts.ci.centos.org/artifacts/rdo/images
+UNDERCLOUD=$URL/$RELEASE/$BUILD_SYS/stable/undercloud.qcow2
+sshcmd='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+scpcmd='scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+
+# Create a non-root user with passwordless sudo permissions on the virthost
+# This is a requirement of the usbkey script, but the CI nodes do not have it
+$sshcmd root@$VIRTHOST <<-EOF
+  useradd stack
+  mkdir /home/stack/.ssh
+  cp /root/.ssh/authorized_keys /home/stack/.ssh/
+  chown -R stack:stack /home/stack
+  echo "Defaults:stack !requiretty" > /etc/sudoers.d/stack
+  echo "stack ALL=(root) NOPASSWD:ALL" >> /etc/sudoers.d/stack
+  chmod 0440 /etc/sudoers.d/stack
+EOF
+
+# Copy artifacts to the CI provided virthost to simulate USB key
+$sshcmd stack@$VIRTHOST <<-EOF
+  if [[ ! -f /tmp/usb/undercloud.qcow2 ]]; then
+    mkdir -p /tmp/usb
+    curl -o /tmp/usb/undercloud.qcow2 $UNDERCLOUD
+    curl -o /tmp/usb/undercloud.qcow2.md5 $UNDERCLOUD.md5
+  else
+    echo "undercloud.qcow2 file was found, skipping download"
+  fi
+EOF
+
+#Ensure rsync is installed on target, required w/ ssh as the protocol
+$sshcmd root@$VIRTHOST <<-EOF
+  yum -y install rsync
+EOF
+
+#$scpcmd -r $WORKSPACE/tripleo-quickstart stack@$VIRTHOST:/tmp/usb/
+rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress $WORKSPACE/tripleo-quickstart stack@$VIRTHOST:/tmp/usb/
+$sshcmd stack@$VIRTHOST "cp /tmp/usb/tripleo-quickstart/ci-scripts/usbkey/* /tmp/usb/"
+$sshcmd stack@$VIRTHOST "cp /tmp/usb/tripleo-quickstart/ci-scripts/usbkey/quickstart-usb.yml /tmp/usb/tripleo-quickstart/playbooks/"
+
+
+# Run the USB script
+$sshcmd stack@$VIRTHOST 'pushd /tmp/usb; ./RUN_ME.sh'
+
+# Support collect logs on the virthost by providing hosts and ssh config
+export ANSIBLE_INVENTORY=$WORKSPACE/hosts
+export SSH_CONFIG=$WORKSPACE/ssh.config.ansible
+export ANSIBLE_SSH_ARGS="-F ${SSH_CONFIG}"
+$scpcmd stack@$VIRTHOST:~/ssh.config* $WORKSPACE/
+$scpcmd stack@$VIRTHOST:~/hosts* $WORKSPACE/
+$scpcmd stack@$VIRTHOST:~/id_* $WORKSPACE/
+sed -i 's,\/home\/stack\/\.quickstart,'"$WORKSPACE"',g' $WORKSPACE/ssh.config.ansible
+bash ci-scripts/collect-logs.sh
+
+
---
+++ ci-scripts/usbkey/RUN_ME.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+######################################################################
+# This script is part of the ooo-quickstart usbkey. It's designed
+# to execute the quickstart from a usbkey on a test machine directly.
+#
+######################################################################
+#Set ansible environmental variables
+source ansible_env
+
+#launch quickstart using the local image against the localhost
+USB_DIR=$PWD
+pushd tripleo-quickstart
+export COMMAND="./quickstart.sh \
+                --no-clone \
+                --requirements ci-scripts/usbkey/usb_requirements.txt \
+                --playbook quickstart-usb.yml \
+                --extra-vars image_cache_dir=$HOME \
+                --extra-vars image_url=file://$USB_DIR/undercloud.qcow2 \
+                localhost"
+
+#check if the current user is root
+if [[ $USER == root ]]; then
+    echo "Use a non-root user with sudo permissions instead of root, exiting"
+    exit
+fi
+
+echo "==================================================================="
+echo "Installing Dependencies"
+sudo ./quickstart.sh --install-deps
+
+echo "==================================================================="
+echo "Running tripelo-quickstart in 15 seconds w/ the following command"
+echo ""
+echo "See https://github.com/openstack/tripleo-quickstart for the latest"
+echo "documenation and source code"
+echo "==================================================================="
+
+echo ""
+echo $COMMAND
+
+sleep 15
+$COMMAND
+
+popd
