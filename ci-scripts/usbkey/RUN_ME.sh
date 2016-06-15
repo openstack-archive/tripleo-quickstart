3637c7537859329a1099d8c80e212fc545f0dce9
Wesley Hayutin <weshayutin@gmail.com>
2016-06-15 09:37:51 -0400

    use the ansible-role-tripleo-inventory to override native inventory
    * This change will include an inventory of the overcloud nodes.
    This will allow ansible to execute on the overcloud nodes and
    collect logs from the entire environment.
    
    Change-Id: I3ea5ea1eba5f0e40e618bd7ae1410c25f8191ddf

--- ci-scripts/usbkey/RUN_ME.sh
+++ ci-scripts/usbkey/RUN_ME.sh
@@ -30,7 +30,6 @@ source $USB_DIR/ansible_env
 pushd $USB_DIR/tripleo-quickstart
 export COMMAND="bash quickstart.sh \
                 --no-clone \
-                --requirements ci-scripts/usbkey/usb_requirements.txt \
                 --playbook quickstart-usb.yml \
                 --extra-vars image_cache_dir=$HOME \
                 --extra-vars undercloud_image_url=file://$USB_DIR/undercloud.qcow2 \
