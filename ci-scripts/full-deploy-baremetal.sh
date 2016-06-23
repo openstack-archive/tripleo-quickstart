6f8f0f42d88281554d8f99f953608fc438fa18d6
Ronelle Landy <rlandy@redhat.com>
2016-06-23 16:05:04 -0400

    Adds ci-script to deploy on baremetal hardware
    The script requires that there is a $HW_ENV_DIR
    where the environment-specific files are kept,
    such as the instackenv.json.
    
    Change-Id: I6b04519750db0b66b58d4e9291001ee698ba28a8

---
+++ ci-scripts/full-deploy-baremetal.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# CI test that does a full deploy on baremetal hardware.
+# $HW_ENV_DIR is the directory where environment-specific files are kept.
+# $REQUIREMENTS_FILE is used to include any additional repositories
+# Usage: tripleo-quickstart-baremetal.sh \
+#        <release> \
+#        <hw-env-dir> \
+#        <network-isolation> \
+#        <requirements-file> \
+#        <config-file> \
+#        <playbook>
+
+set -eux
+
+RELEASE=$1
+HW_ENV_DIR=$2
+NETWORK_ISOLATION=$3
+REQUIREMENTS_FILE=$4
+CONFIG_FILE=$5
+PLAYBOOK=$6
+
+pushd $WORKSPACE/tripleo-quickstart
+bash quickstart.sh \
+--ansible-debug \
+--bootstrap \
+--working-dir $WORKSPACE/ \
+--tags all \
+--no-clone \
+--requirements quickstart-role-requirements.txt \
+--requirements $WORKSPACE/$HW_ENV_DIR/network_configs/$NETWORK_ISOLATION/requirements_files/$REQUIREMENTS_FILE \
+--config $WORKSPACE/$HW_ENV_DIR/network_configs/$NETWORK_ISOLATION/config_files/$CONFIG_FILE \
+--extra-vars @$WORKSPACE/$HW_ENV_DIR/network_configs/$NETWORK_ISOLATION/env_settings.yml \
+--playbook $PLAYBOOK \
+--extra-vars undercloud_instackenv_template=$WORKSPACE/$HW_ENV_DIR/instackenv.json \
+--extra-vars network_environment_file=$WORKSPACE/$HW_ENV_DIR/network_configs/$NETWORK_ISOLATION/${NETWORK_ISOLATION}.yml \
+--release $RELEASE
+$VIRTHOST
+
+popd
+
