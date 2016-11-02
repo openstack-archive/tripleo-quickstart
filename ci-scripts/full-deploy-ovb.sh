adb6f8a80c2424c5ac4e2f014ab7e731d10f2bce
Ronelle Landy <rlandy@redhat.com>
2016-11-02 12:08:30 -0400

    Add roles-gate playbook run to OVB ci-script
    The OVB gate was not picking up Gerrit changes.
    This review adds the role-gate run to the ci-script
    to incorporate the Gerrit change under test in the
    Jenkins job run.
    
    Change-Id: I3d733665d3b3819313cd7d2fc2615421263c71e6

--- ci-scripts/full-deploy-ovb.sh
+++ ci-scripts/full-deploy-ovb.sh
@@ -5,10 +5,11 @@
 #        <release> \
 #        <hw-env-dir> \
 #        <network-isolation> \
-#        <config-file> \
+#        <config> \
 #        <ovb-settings-file> \
 #        <ovb-creds-file>  \
-#        <playbook>
+#        <playbook> \
+#        <job-type>
 
 set -eux
 
@@ -17,22 +18,46 @@ set -eux
 RELEASE=$1
 HW_ENV_DIR=$2
 NETWORK_ISOLATION=$3
-CONFIG_FILE=$4
+CONFIG=$4
 OVB_SETTINGS_FILE=$5
 OVB_CREDS_FILE=$6
 PLAYBOOK=$7
+JOB_TYPE=$8
 
 socketdir=$(mktemp -d /tmp/sockXXXXXX)
 export ANSIBLE_SSH_CONTROL_PATH=$socketdir/%%h-%%r
 
+# preparation steps to run with the gated roles
+if [ "$JOB_TYPE" = "roles-gate" ] || [ "$JOB_TYPE" = "gate" ]; then
+    # set up the gated repos and modify the requirements file to use them
+    bash quickstart.sh \
+        --working-dir $WORKSPACE/ \
+        --no-clone \
+        --bootstrap \
+        --requirements quickstart-extras-requirements.txt \
+        --playbook gate-roles.yml \
+        --release ${CI_ENV:+$CI_ENV/}$RELEASE${REL_TYPE:+-$REL_TYPE} \
+        $OPT_ADDITIONAL_PARAMETERS \
+        localhost
+    # once more to let the gating role be gated as well
+    bash quickstart.sh \
+        --working-dir $WORKSPACE/ \
+        --no-clone \
+        --bootstrap \
+        --requirements quickstart-extras-requirements.txt \
+        --playbook gate-roles.yml \
+        --release ${CI_ENV:+$CI_ENV/}$RELEASE${REL_TYPE:+-$REL_TYPE} \
+        $OPT_ADDITIONAL_PARAMETERS \
+        localhost
+fi
+
 bash quickstart.sh \
-    --ansible-debug \
     --bootstrap \
     --working-dir $WORKSPACE/ \
     --tags all \
     --no-clone \
     --requirements quickstart-extras-requirements.txt \
-    --config $WORKSPACE/$HW_ENV_DIR/network_configs/$NETWORK_ISOLATION/config_files/$CONFIG_FILE \
+    --config $WORKSPACE/$HW_ENV_DIR/network_configs/$NETWORK_ISOLATION/config_files/$CONFIG \
     --extra-vars @$OVB_SETTINGS_FILE \
     --extra-vars @$OVB_CREDS_FILE \
     --extra-vars @$WORKSPACE/$HW_ENV_DIR/network_configs/$NETWORK_ISOLATION/env_settings.yml \
