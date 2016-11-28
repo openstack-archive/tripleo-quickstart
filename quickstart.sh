2134e784e57fb35ee604cd2964437a0a0d7d84d5
Gabriele Cerami <gcerami@redhat.com>
2016-11-28 16:50:08 +0100

    main script: Use zuul cloner if present to fetch an pip install extras
    To start taking advantage of zuul capabilities while testing in
    upstream openstack (e.g. Depends-On), we'll have to use zuul cloner when
    present to let it download and install quickstart-extras with the proper
    REF.
    
    Change-Id: Ic9c9a5cf7ed94b59bab27c7a8a4429bf57fd2d09

--- quickstart.sh
+++ quickstart.sh
@@ -126,6 +126,18 @@ bootstrap () {
         # way to make setuptools not try to write the .eggs dir.
         sed -i "s%os.curdir%\'$OPT_WORKDIR\'%" $OPT_WORKDIR/lib/python2.7/site-packages/setuptools/dist.py
         python setup.py install egg_info --egg-base $OPT_WORKDIR
+        if [ -x "$ZUUL_CLONER" && ! -z "$ZUUL_BRANCH" ]; then
+            mkdir -p .tmp
+            EXTRAS_DIR=$(/bin/mktemp -d -p $(pwd)/.tmp)
+            pushd $EXTRAS_DIR
+                $ZUUL_CLONER --cache-dir \
+                    /opt/git \
+                    git://git.openstack.org \
+                    openstack/tripleo-quickstart-extras
+                cd openstack/tripleo-quickstart-extras
+                pip install  .
+            popd
+        fi
         # Handle the case that pip is too old to use a cache-dir
         pip install --no-cache-dir "${OPT_REQARGS[@]}"
     popd
